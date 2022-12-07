3304622b83db7665baef74012a9af67703999e82
TerrificTable <omegagaming189@gmail.com>
2022-12-07 19:39:04 +0100

    draw current time when `main.cpp/displaytime` is true (only gets actual time when connected to wifi)

--- src/screen.hpp
+++ src/screen.hpp
@@ -53,27 +53,49 @@ void draw(std::vector<Value> *in, int avg, int16_t color) {
         (*in).erase((*in).begin()); // remove first aka oldest entry in array/vector 
     }
 
+    int16_t TOP           = 0;
+    if (displaytime) {
+        TOP = 16;
+    }
+
     int16_t MAX           = 1; // Highest co2 value in current array
-    static int16_t HEIGHT = 64; // the screen height
+    static int16_t HEIGHT = 64 - TOP; // the screen height
     for (unsigned int i=0; i < (*in).size(); i++ ) // get highest co2 value
         if ((*in)[i].co2 >= MAX) MAX = (*in)[i].co2; // check if value is higher than current highest, if yes update else, do nothing
     
     int16_t lastI = 0; // variables for last values
-    int16_t lastY = HEIGHT - ((*in)[0].co2 * HEIGHT) / MAX; // variables for last values
+    int16_t lastY = (HEIGHT - ((*in)[0].co2 * HEIGHT) / MAX) + TOP; // variables for last values
     for (unsigned int i=0; i < (*in).size(); i++ ) { // iterate over Value array/vector
         Value val = (*in)[i]; // local `val` has the value of current index in array/vector
-        int16_t y = HEIGHT - (val.co2 * HEIGHT) / MAX; // calculate on what hight to draw a pixel
+        int16_t y = (HEIGHT - (val.co2 * HEIGHT) / MAX) + TOP; // calculate on what hight to draw a pixel
         display.drawLine(lastI, lastY, i, y, color); // draw line from last point to new point
         lastI = i; // update values
         lastY = y; // update values
     }
-    
+
     display.setTextColor(WHITE); // set text color
     display.setTextSize(2); // set text size
     float x = (SCREEN_WIDTH/2) - (String("Co2: " + String(avg)).length() * 6); // get width of `Co2: <current verage co2 value>`
-    display.setCursor(x, SCREEN_HEIGHT/2); // set cursor to 0,0
+    display.setCursor(x, SCREEN_HEIGHT/2 + TOP/2); // set cursor to 0,0
     display.println("Co2: " + String(avg)); // print text to display starting at position 0,0
-    
+
+
+    if (displaytime) {
+        display.setTextColor(WHITE);
+        display.setTextSize(1);
+        time_t now = time(nullptr);
+        struct tm *ti = localtime(&now);
+        String currtime = (ti->tm_hour < 10 ? "0" + String(ti->tm_hour) : String(ti->tm_hour)) + ":" 
+                        + (ti->tm_min  < 10 ? "0" + String(ti->tm_min)  : String(ti->tm_min))  + ":" 
+                        + (ti->tm_sec  < 10 ? "0" + String(ti->tm_sec)  : String(ti->tm_sec));
+        display.setCursor(128/2 - (currtime.length() * 3), 4);
+        display.println(currtime);
+
+        display.drawLine(0, TOP-2, 128, TOP-2, WHITE);
+        display.drawLine(0, TOP-3, 128, TOP-3, WHITE);
+    }
+
+
     display.display(); // update display
 }
 
